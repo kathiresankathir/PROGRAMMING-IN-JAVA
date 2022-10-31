48ba026819bc63aabcdcd5f3eacac6e977d93fa6
Kathir <112713802+kathiresankathir@users.noreply.github.com>
2022-10-31 08:59:30 +0530

    Update perfectsqr.java

--- perfectsqr.java
+++ perfectsqr.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 import java.math.*;
-public class Perfect_sqr {
+public class Perfectsq {
  public static void main(String[] args) {
  try{
  Scanner s=new Scanner(System.in);
- System.out.println("Enter an number");
+ System.out.println("Enter a number");
  int n=s.nextInt();
  double y=Math.sqrt(n);
- System.out.print("The answer is="+y+", ");
+ System.out.print("The answer is"+y+", ");
  System.out.println(-y);
  }
  catch(Exception e)
